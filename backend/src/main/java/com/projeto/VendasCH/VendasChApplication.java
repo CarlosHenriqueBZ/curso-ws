23fbe36a792c1ac77b70c66b6fbad2ac591cf407
Carlos Henrique <chbezerra25@gmail.com>
2021-11-03 20:22:35 -0300

    Projeto Inicio

---
+++ backend/src/main/java/com/projeto/VendasCH/VendasChApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.VendasCH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasChApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasChApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projeto/VendasCH/VendasChApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.VendasCH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasChApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
